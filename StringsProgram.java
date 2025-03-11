7d962238f2cb6d4fc952d1ed3467c379ae0a4ac1
Hibatullah Syauqi At Taqi <108654340+hibatullahsyauqi@users.noreply.github.com>
2025-03-11 21:07:48 +0700

    string and added some folder into .gitignore

---
+++ StringsProgram.java
@@ -0,0 +1,13 @@
+public class StringsProgram {
+    public static void main(String[] args) {
+        System.out.println("Madlibs!");
+
+        String username = "lollipopcycle";
+        System.out.println("My ____ sister accidentally ____ed all over her new ____");
+        String adjective = "flamboyant";
+        String verb = "fart";
+        String noun = "pogo stick";
+
+        System.out.println("My " + adjective + " sister accidentally " + verb + "ed all over her new " + noun);
+    }
+}
