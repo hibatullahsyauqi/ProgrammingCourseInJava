f33caf3ef69bc0cf48623e5e2ed0c717d32bcd13
Hibatullah Syauqi At Taqi <108654340+hibatullahsyauqi@users.noreply.github.com>
2025-03-12 20:20:28 +0700

    added an arithmetic thingy

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,21 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        int z;
+
+//        z = x + y; // addition
+//        z = x - y; // subtraction
+//        z = x * y; // multiplication
+//        z = x / y; // division
+//        z = x % y; // modulus
+
+        // augmented assignment
+//        x += y; // x = x + y
+//        x *= y; // x = x * y
+
+        // increment and decrement
+//        x++; // incrementing by one
+//        x--; // decrementing by one
+    }
+}
