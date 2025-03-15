eb5e3114ccde804514e66af43df806f516ffe6f9
Hibatullah Syauqi At Taqi <108654340+hibatullahsyauqi@users.noreply.github.com>
2025-03-15 14:26:04 +0700

    array of objects

---
+++ src/com/myapps/arrayofobjects/Main.java
@@ -0,0 +1,16 @@
+package com.myapps.arrayofobjects;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mustang", "Black");
+        Car car2 = new Car("Corvette", "Teal");
+        Car car3 = new Car("Charger", "Yellow");
+        
+        Car[] cars = {car1, car2, car3};
+
+        for (Car car : cars) {
+            car.drive();
+        }
+    }
+}
