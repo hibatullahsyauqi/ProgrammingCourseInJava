48f1baeef8b13c41d0937acc160cc409b6ee9a65
Hibatullah Syauqi At Taqi <108654340+hibatullahsyauqi@users.noreply.github.com>
2025-03-18 22:38:52 +0700

    started a new path in oop!

---
+++ src/com/object_oriented_java/Class1.java
@@ -0,0 +1,28 @@
+package com.object_oriented_java;
+// membuat class sebagai template
+
+class Mahasiswa {
+    String name;
+    String idCard;
+    String major;
+    double gpa;
+    int age;
+}
+
+public class Class1 {
+    public static void main(String[] args) {
+        // instansiasi object
+
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+
+        // kita bisa loh punya mahasiswa2, 3, 4, dll!
+        mahasiswa1.name = "Dale";
+        mahasiswa1.idCard = "12345";
+        mahasiswa1.major = "Physics";
+        mahasiswa1.gpa = 3.45;
+        mahasiswa1.age = 19;
+        System.out.println(mahasiswa1.name);
+
+        // kinda hard right? we have a solution for that, using constructor!!!
+    }
+}
